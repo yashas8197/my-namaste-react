dca1c7512fdbb861be64b7eede2cefbd6aecc468
yashas8197 <yashasjr7@gmail.com>
2023-08-06 15:55:06 +0530

    Added comments to explain the creation and rendering of React elements

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
+// Create a parent React element with an 'id' attribute of 'parent'
 const parent = React.createElement("div", { id: "parent" }, [
+  // Create a child div with an 'id' attribute of 'child'
   React.createElement("div", { id: "child" }, [
+    // Create an 'h1' element with the text "I'm a h1 tag"
     React.createElement("h1", {}, "I'm a h1 tag"),
+    // Create an 'h2' element with the text "i'm a h2 tag"
     React.createElement("h2", {}, "i'm a h2 tag"),
   ]),
+  // Create another child div with an 'id' attribute of 'child2'
   React.createElement("div", { id: "child2" }, [
+    // Create an 'h1' element with the text "I'm a h1 tag"
     React.createElement("h1", {}, "I'm a h1 tag"),
+    // Create an 'h2' element with the text "i'm a h2 tag"
     React.createElement("h2", {}, "i'm a h2 tag"),
   ]),
 ]);
 
+// Find the container element in the HTML and create a React root to render the components
 const root = ReactDOM.createRoot(document.getElementById("container"));
 
+// Render the parent element using the React root
 root.render(parent);
