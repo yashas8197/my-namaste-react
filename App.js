f8b03565df1346f131749596e7987ef3d0b01f8b
yashas8197 <yashasjr7@gmail.com>
2023-08-08 23:56:25 +0530

    episode-2

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 // Create a parent React element with an 'id' attribute of 'parent'
 const parent = React.createElement("div", { id: "parent" }, [
   // Create a child div with an 'id' attribute of 'child'
   React.createElement("div", { id: "child" }, [
     // Create an 'h1' element with the text "I'm a h1 tag"
-    React.createElement("h1", {}, "I'm a h1 tag"),
+    React.createElement("h1", {}, "This is namaste React"),
     // Create an 'h2' element with the text "i'm a h2 tag"
-    React.createElement("h2", {}, "i'm a h2 tag"),
+    React.createElement("h2", {}, "by Akshay saini"),
   ]),
   // Create another child div with an 'id' attribute of 'child2'
   React.createElement("div", { id: "child2" }, [
