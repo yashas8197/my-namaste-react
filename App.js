fa9a4765e18c47cad6663a8a5ad1b583064c5ac5
yashas8197 <yashasjr7@gmail.com>
2023-08-20 19:33:11 +0530

    creating element through React and JSX

--- App.js
+++ App.js
@@ -1,26 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// Create a parent React element with an 'id' attribute of 'parent'
-const parent = React.createElement("div", { id: "parent" }, [
-  // Create a child div with an 'id' attribute of 'child'
-  React.createElement("div", { id: "child" }, [
-    // Create an 'h1' element with the text "I'm a h1 tag"
-    React.createElement("h1", {}, "This is namaste React"),
-    // Create an 'h2' element with the text "i'm a h2 tag"
-    React.createElement("h2", {}, "by Akshay saini"),
-  ]),
-  // Create another child div with an 'id' attribute of 'child2'
-  React.createElement("div", { id: "child2" }, [
-    // Create an 'h1' element with the text "I'm a h1 tag"
-    React.createElement("h1", {}, "I'm a h1 tag"),
-    // Create an 'h2' element with the text "i'm a h2 tag"
-    React.createElement("h2", {}, "i'm a h2 tag"),
-  ]),
-]);
+// React.creatElement => Object => HTMLElement(render)
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Namaste React 🚀"
+);
 
-// Find the container element in the HTML and create a React root to render the components
+console.log(heading);
+
+// JSX - HTML-like syntax but not HTML
+
+const jsxHeading = <h1 id="heading">Namaste React using JSX 🚀</h1>;
+
+console.log(jsxHeading);
+
 const root = ReactDOM.createRoot(document.getElementById("container"));
 
-// Render the parent element using the React root
-root.render(parent);
+root.render(jsxHeading);
+
+// root.render(heading);
