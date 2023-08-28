053383e4f27a0d263446abd0f849dbfd01539c91
yashas8197 <yashasjr7@gmail.com>
2023-08-28 16:14:31 +0530

    code in jsx

--- App.js
+++ App.js
@@ -1,23 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React.creatElement => Object => HTMLElement(render)
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Namaste React 🚀"
-);
-
-console.log(heading);
-
 // JSX - HTML-like syntax but not HTML
-
-const jsxHeading = <h1 id="heading">Namaste React using JSX 🚀</h1>;
-
-console.log(jsxHeading);
+//JSX => React.createElement => ReactElement-JS Object => HTMLElement(render)
+const heading = <h1 className="heading">Namaste React using JSX 🚀</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById("container"));
 
-root.render(jsxHeading);
-
-// root.render(heading);
+root.render(heading);
