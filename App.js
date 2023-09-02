95d46ea8172839192f363bf14b40fa6cf02e9002
yashas8197 <yashasjr7@gmail.com>
2023-09-02 10:18:54 +0530

    hard coded data to cards

--- App.js
+++ App.js
@@ -1,24 +1,86 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Title = () => <h1 className="head">Namaste React 🚀</h1>;
+/**
+ *
+ * Header
+ *  - Logo
+ *  - Nav Items
+ * Body
+ *  - Search
+ *  - RestaurantContainer
+ *    - RestaurantCard
+ * Footer
+ *  - CopyRight
+ *  - Links
+ *  - Address
+ *  - Contact
+ *
+ *
+ */
 
-const Title2 = function () {
-  return <h2>heading using function keyword</h2>;
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://i.pinimg.com/originals/b1/fc/bb/b1fcbbfd4fb8116c714ef352bb39bbaf.jpg"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
-//function component
-//component composition
-const HeadingComponent = () => (
-  <div id="containerOfH1">
-    <Title />
-    {Title()}
-    <Title></Title>
-    <h1 className="heading">Namaste React functional component</h1>
-    <Title2 />
-  </div>
-);
+const ResturentCard = ({ resName, cuisines, starRating }) => {
+  return (
+    <div className="res-card">
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src="https://b.zmtcdn.com/data/pictures/chains/1/50691/0435a03f4d2017a0a64d90b279c2fa63.jpg"
+      />
+      <h3>{resName}</h3>
+      <h4>{cuisines}</h4>
+      <h4>{starRating}</h4>
+      <h4>38 minutes</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div>
+      <div className="search">Search</div>
+      <div className="res-container">
+        <ResturentCard
+          resName="Meghana Foods"
+          cuisines="Biriyani , North Indian , Asian"
+          starRating="4.4"
+        />
+        <ResturentCard resName="KFC" cuisines="Burgers" starRating="4.3" />
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("container"));
 
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
