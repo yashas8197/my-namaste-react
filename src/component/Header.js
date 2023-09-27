248fe38fbc50170ea09d4477f1d78d3f835c9fb4
yashas8197 <yashasjr7@gmail.com>
2023-09-27 15:28:51 +0530

    feat: added button

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,6 +1,8 @@
 import { LOGO_URL } from "../utils/constant";
+import { useState } from "react";
 
 const Header = () => {
+  const [btnNameReact, setBtnNameReact] = useState("Login");
   return (
     <div className="header">
       <div className="logo-container">
@@ -12,6 +14,16 @@ const Header = () => {
           <li>About Us</li>
           <li>Contact Us</li>
           <li>Cart</li>
+          <button
+            className="login-btn"
+            onClick={() => {
+              btnNameReact === "Login"
+                ? setBtnNameReact("Logout")
+                : setBtnNameReact("Login");
+            }}
+          >
+            {btnNameReact}
+          </button>
         </ul>
       </div>
     </div>
