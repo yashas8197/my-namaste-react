1b87c053ac6f6a3cc9c1cc86530502c4188cea09
yashas8197 <yashasjr7@gmail.com>
2023-10-18 19:11:00 +0530

    used setState to update state variable

--- src/component/About.js
+++ src/component/About.js
@@ -5,7 +5,6 @@ const About = () => {
   return (
     <div>
       <h1>its a about page</h1>
-      <User name={"yashas(function)"} location={"udupi(function)"} />
       <UserClass name={"Yashas(class)"} location={"Udupi(class)"} />
     </div>
   );
--- src/component/UserClass.js
+++ src/component/UserClass.js
@@ -6,16 +6,23 @@ class UserClass extends React.Component {
 
     this.state = {
       count: 0,
-      count2: 2,
     };
   }
   render() {
     const { name, location } = this.props;
-    const { count, count2 } = this.state;
+    const { count } = this.state;
     return (
       <div className="user-card">
         <h1>Count: {count}</h1>
-        <h1>Count2: {count2}</h1>
+        <button
+          onClick={() => {
+            this.setState({
+              count: this.state.count + 1,
+            });
+          }}
+        >
+          counter
+        </button>
         <h2>Name: {name}</h2>
         <h3>Location: {location}</h3>
         <h3>contact: @yashas8197</h3>
