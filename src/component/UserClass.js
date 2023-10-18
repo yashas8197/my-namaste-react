e26348d4defb58114226d0058e07ebd9b1cc4a97
yashas8197 <yashasjr7@gmail.com>
2023-10-18 18:55:28 +0530

    used state variable in class component

--- src/component/UserClass.js
+++ src/component/UserClass.js
@@ -3,11 +3,19 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      count: 0,
+      count2: 2,
+    };
   }
   render() {
     const { name, location } = this.props;
+    const { count, count2 } = this.state;
     return (
       <div className="user-card">
+        <h1>Count: {count}</h1>
+        <h1>Count2: {count2}</h1>
         <h2>Name: {name}</h2>
         <h3>Location: {location}</h3>
         <h3>contact: @yashas8197</h3>
