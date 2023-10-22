f1c16da5ba4cd231c331bbcfa212d7d5de1e15a9
yashas8197 <yashasjr7@gmail.com>
2023-10-22 20:53:59 +0530

    feat: added online or offline feature

--- src/component/Body.js
+++ src/component/Body.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import RestaurantCard from "./RestaurantCard";
 import Shimmer from "./Shimmer";
 import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Body = () => {
   const [listOfReastaurants, setListOfRestaurants] = useState([]);
@@ -28,6 +29,13 @@ const Body = () => {
       json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
   };
+
+  const onlineStatus = useOnlineStatus();
+
+  if (onlineStatus === false)
+    return (
+      <h1>Looks like you're offline! Please check your internet connection</h1>
+    );
 
   return listOfReastaurants.length === 0 ? (
     <Shimmer />
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,9 +1,12 @@
 import { LOGO_URL } from "../utils/constant";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Header = () => {
   const [btnNameReact, setBtnNameReact] = useState("Login");
+
+  const onlineStatus = useOnlineStatus();
   return (
     <div className="header">
       <div className="logo-container">
@@ -11,6 +14,7 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
+          <li>Online Status: {onlineStatus ? "🟢" : "🔴"}</li>
           <li>
             <Link to="/">Home</Link>
           </li>
